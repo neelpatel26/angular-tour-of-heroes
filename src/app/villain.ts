00549b577a8d79e7c3d948bcbae98667b06a476d
Neel Patel <neeltp26@gmail.com>
2019-06-26 19:51:04 -0700

    my first git commit

---
+++ src/app/villain.ts
@@ -0,0 +1,10 @@
+export class Villain {
+    id: number;
+    name: string;
+    howUgly: number; //rate from 1 to 5 where 5 being highest ugly
+  }
+
+export class MainVillain {
+    name: string;
+    howUgly: number; //rate from 1 to 5 where 5 being highest ugly
+  }
